c45a7bcd153c21b534fdbf0cb14ad328d5eade0d
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2025-04-23 11:51:38 +0800

    using only XDR client

--- custom_components/tplink_router/tplinkrouterc6u/provider.py
+++ custom_components/tplink_router/tplinkrouterc6u/provider.py
@@ -1,26 +1,17 @@
 from logging import Logger
 
-
-from .common.exception import ClientException
-from .client.c6u import TplinkRouter
-from .client.deco import TPLinkDecoClient
-from .client_abstract import AbstractRouter
-from .client.mr import TPLinkMRClient
-from .client.ex import TPLinkEXClient
-from .client.c5400x import TplinkC5400XRouter
 from .client.c1200 import TplinkC1200Router
-from .client.c80 import TplinkC80Router
 from .client.vr import TPLinkVRClient
 from .client.xdr import TPLinkXDRClient
-from .client.wdr import TplinkWDRRouter
+from .client_abstract import AbstractRouter
+from .common.exception import ClientException
 
 
 class TplinkRouterProvider:
     @staticmethod
     def get_client(host: str, password: str, username: str = 'admin', logger: Logger = None,
                    verify_ssl: bool = True, timeout: int = 30) -> AbstractRouter:
-        for client in [TPLinkVRClient, TPLinkEXClient, TPLinkMRClient, TPLinkDecoClient,
-                       TPLinkXDRClient, TplinkRouter, TplinkC80Router, TplinkWDRRouter, TplinkC5400XRouter]:
+        for client in [TPLinkXDRClient]:
             router = client(host, password, username, logger, verify_ssl, timeout)
             if router.supports():
                 return router
